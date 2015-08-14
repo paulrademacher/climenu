4a210919e4f7bf4cb78123450c1c50dd4765d872
Paul Rademacher <paul.rademacher@gmail.com>
2015-08-14 14:06:33 -0700

    getchar comments

--- getchar.go
+++ getchar.go
@@ -2,6 +2,7 @@ package climenu
 
 import "github.com/pkg/term"
 
+// Returns either an ascii code, or (if input is an arrow) a Javascript key code.
 func getChar() (ascii int, keyCode int, err error) {
 	t, _ := term.Open("/dev/tty")
 	term.RawMode(t)
@@ -15,13 +16,19 @@ func getChar() (ascii int, keyCode int, err error) {
 	if numRead == 3 && bytes[0] == 27 && bytes[1] == 91 {
 		// Three-character control sequence, beginning with "ESC-[".
 
+		// Since there are no ASCII codes for arrow keys, we use
+		// Javascript key codes.
 		if bytes[2] == 65 {
+			// Up
 			keyCode = 38
 		} else if bytes[2] == 66 {
+			// Down
 			keyCode = 40
 		} else if bytes[2] == 67 {
+			// Right
 			keyCode = 39
 		} else if bytes[2] == 68 {
+			// Left
 			keyCode = 37
 		}
 	} else if numRead == 1 {
